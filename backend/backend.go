90a275ef468a0f5ec7b24e5c8abba520131ba21a
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-09-20 15:07:06 -0700

    Rewrite backend to use echo

--- backend/backend.go
+++ backend/backend.go
@@ -1,59 +1,43 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"log"
 	"net/http"
+
+	"github.com/go-playground/validator"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	const port = 8000
-	fmt.Printf("Starting server at http://localhost:%d\n", port)
-
-	http.HandleFunc("/decode", decodeHandler)
-
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil); err != nil {
-		log.Fatal(err)
-	}
+	e := echo.New()
+	e.Validator = &CustomValidator{validator: validator.New()}
+	e.POST("/decode", decodeHandler)
+	e.Logger.Fatal(e.Start(":8000"))
 }
 
-type decodeRequest struct {
-	Value string `json:"value"`
-	Key   string `json:"key"`
+type DecodeRequest struct {
+	Value string `json:"value" validate:"required"`
+	Key   string `json:"key" validate:"required"`
 }
 
-func decodeHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		http.Error(w, "Expected POST method", http.StatusMethodNotAllowed)
-		return
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
+
+func decodeHandler(c echo.Context) error {
+	var req DecodeRequest
+	if err := c.Bind(&req); err != nil {
+		return err
+	}
+	if err := c.Validate(&req); err != nil {
+		return err
 	}
 
-	if r.Header.Get("Content-Type") != "application/json" {
-		http.Error(w, "Expected Content-Type to be application/json", http.StatusUnsupportedMediaType)
-		return
-	}
-
-	r.Body = http.MaxBytesReader(w, r.Body, 1048576)
-
-	dec := json.NewDecoder(r.Body)
-	dec.DisallowUnknownFields()
-
-	var decodeReq decodeRequest
-	if err := dec.Decode(&decodeReq); err != nil {
-		if err.Error() == "http: request body too large" {
-			http.Error(w, "Request body must not be larger than 1MB", http.StatusRequestEntityTooLarge)
-		} else {
-			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-		}
-		return
-	}
-
-	if err := dec.Decode(&struct{}{}); !errors.Is(err, io.EOF) {
-		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
-	}
-
-	fmt.Fprintf(w, "%s", DecodeCipher(decodeReq.Value, decodeReq.Key))
+	return c.String(http.StatusOK, DecodeCipher(req.Value, req.Key))
 }
